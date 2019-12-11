120f55717918f7e0237af0a55d5aa797a3dbcfcb
zzzl <1946298407@qq.com>
2019-12-11 09:08:57 +0800

    删除idea“

--- common/src/main/java/com/dingtao/common/core/hello/IBaseModel.java
+++
@@ -1,8 +0,0 @@
-package com.dingtao.common.core.hello;
-
-/**
- * 基类model
- */
-public interface IBaseModel {
-
-}
--- common/src/main/java/com/dingtao/common/core/hello/IBaseView.java
+++
@@ -1,9 +0,0 @@
-package com.dingtao.common.core.hello;
-
-/**
- * base view
- */
-public interface IBaseView {
-
-    BasePresenter initPresenter();//初始化presennter方法
-}
